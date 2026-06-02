5e43834c765d562ab8945feabe5024b20d60111c
JMarlon <jmarloncin07@gmail.com>
2026-06-02 11:01:46 -0500

    feat(config): added database connection

--- src/main/java/com/biblioteca/config/ConnectionDB.java
+++ src/main/java/com/biblioteca/config/ConnectionDB.java
@@ -1,5 +1,31 @@
 package com.biblioteca.config;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class ConnectionDB {
+
+    private static Connection connection;
+
+    public static Connection getConnection() {
+        try {
+            if (connection == null || connection.isClosed()) {
+                Dotenv dotenv = Dotenv.load();
+
+                String url = dotenv.get("DB_URL");
+                String user = dotenv.get("DB_USER");
+                String password = dotenv.get("DB_PASSWORD");
+
+                connection = DriverManager.getConnection(url, user, password);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
 
 }
