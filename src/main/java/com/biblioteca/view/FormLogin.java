3b125d1648097f58ef68c25f18928e53ad2f68d9
JMarlon <jmarloncin07@gmail.com>
2026-06-08 16:14:30 -0500

    feat(view): login form view added

--- src/main/java/com/biblioteca/view/FormLogin.java
+++ src/main/java/com/biblioteca/view/FormLogin.java
@@ -1,5 +1,68 @@
 package com.biblioteca.view;
 
-public class FormLogin {
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class FormLogin extends JPanel {
+
+    public FormLogin() {
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        this.setPreferredSize(new Dimension(400, 400));
+
+        JLabel title = new JLabel("Login");
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+        title.setFont(new Font("Arial", Font.BOLD, 50));
+
+        JLabel email = new JLabel("Your e-mail");
+        email.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JTextField inputEmail = new JTextField();
+
+        inputEmail.setMaximumSize(new Dimension(200, 30));
+        inputEmail.setPreferredSize(new Dimension(200, 30));
+
+        JLabel password = new JLabel("Your password");
+        password.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JTextField inputPassword = new JTextField();
+        inputPassword.setMaximumSize(new Dimension(200, 30));
+        inputPassword.setPreferredSize(new Dimension(200, 30));
+
+        JLabel createdAccount = new JLabel();
+        createdAccount.setText("Do not have an account? Create one");
+        createdAccount.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JButton btn = new JButton("Continuar");
+
+        btn.setAlignmentX(Component.CENTER_ALIGNMENT);
+        btn.setFocusPainted(false);
+        btn.setMaximumSize(new Dimension(150, 30));
+        btn.setPreferredSize(new Dimension(150, 30));
+
+        this.add(Box.createVerticalGlue());
+        this.add(title);
+        this.add(Box.createRigidArea(new Dimension(0, 50)));
+        this.add(email);
+        this.add(Box.createRigidArea(new Dimension(0, 5)));
+        this.add(inputEmail);
+        this.add(Box.createRigidArea(new Dimension(0, 20)));
+        this.add(password);
+        this.add(Box.createRigidArea(new Dimension(0, 5)));
+        this.add(inputPassword);
+        this.add(Box.createRigidArea(new Dimension(0, 45)));
+        this.add(createdAccount);
+        this.add(Box.createRigidArea(new Dimension(0, 20)));
+        this.add(btn);
+        this.add(Box.createRigidArea(new Dimension(0, 10)));
+        this.add(Box.createVerticalGlue());
+
+    }
 }
