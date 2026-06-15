e500606e87a409490fb4cad5abb4d6c4ecc48b4d
JMarlon <jmarloncin07@gmail.com>
2026-06-15 14:11:13 -0500

    refactor(librarian-model): the functionalities of a librarian were updated

--- src/main/java/com/biblioteca/model/LibrarianModel.java
+++ src/main/java/com/biblioteca/model/LibrarianModel.java
@@ -1,5 +1,39 @@
 package com.biblioteca.model;
 
+import java.util.List;
+
+import com.biblioteca.entity.Librarian;
+import com.biblioteca.repository.LibrarianRepository;
+
 public class LibrarianModel {
 
+    private final LibrarianRepository repository;
+
+    public LibrarianModel(LibrarianRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Librarian> getAllLibrarians() {
+        return repository.findAll();
+    }
+
+    public void saveLibrarian(Librarian librarian) {
+        repository.save(librarian);
+    }
+
+    public void updateLibrarian(Librarian librarian) {
+        repository.update(librarian);
+    }
+
+    public void deleteLibrarian(String email) {
+        repository.delete(email);
+    }
+
+    public boolean existsByEmail(String email) {
+        return repository.existsByEmail(email);
+    }
+
+    public Librarian findByEmail(String email) {
+        return repository.findByEmail(email);
+    }
 }
