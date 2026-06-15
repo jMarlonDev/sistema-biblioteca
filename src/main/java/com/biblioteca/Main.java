c1c06c21a93e915eb9046bef73781609e66b19fc
JMarlon <jmarloncin07@gmail.com>
2026-06-15 14:58:49 -0500

    chore: the user view was added

--- src/main/java/com/biblioteca/Main.java
+++ src/main/java/com/biblioteca/Main.java
@@ -7,15 +7,18 @@ import javax.swing.SwingUtilities;
 import com.biblioteca.config.ConnectionDB;
 import com.biblioteca.controller.LibrarianController;
 import com.biblioteca.controller.LoginController;
-import com.biblioteca.controller.RegisterController; // <- Añadido
+import com.biblioteca.controller.RegisterController;
+import com.biblioteca.controller.UserController;
 import com.biblioteca.model.LibrarianModel;
 import com.biblioteca.model.LoginModel;
-import com.biblioteca.model.RegisterModel; // <- Añadido
+import com.biblioteca.model.RegisterModel;
+import com.biblioteca.model.UserModel;
 import com.biblioteca.repository.AdministratorRepositoryImpl;
 import com.biblioteca.repository.LibrarianRepositoryImpl;
 import com.biblioteca.repository.UserRepositoryImpl;
 import com.biblioteca.view.LibrarianManagementView;
-import com.biblioteca.view.MainWindow; // <- Añadido
+import com.biblioteca.view.MainWindow;
+import com.biblioteca.view.UserManagementView;
 
 public class Main {
 
@@ -32,6 +35,7 @@ public class Main {
             RegisterModel registerModel = new RegisterModel(userRepo);
 
             LibrarianModel librarianModel = new LibrarianModel(librarianRepo);
+            UserModel userModel = new UserModel(userRepo);
 
             MainWindow mainWindow = new MainWindow();
 
@@ -40,6 +44,11 @@ public class Main {
             new LibrarianController(librarianView, librarianModel);
 
             mainWindow.getMenuAdministrator().addContentPanel(librarianView, "Manage Librarians");
+
+            UserManagementView userView = new UserManagementView();
+            new UserController(userView, userModel);
+
+            mainWindow.getMenuAdministrator().addContentPanel(userView, "Manage Users");
 
             new LoginController(
                     mainWindow.getFormLogin(),
