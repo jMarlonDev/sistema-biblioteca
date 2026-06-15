4f95be37689153bbdbe1f3066567b815c43fb979
JMarlon <jmarloncin07@gmail.com>
2026-06-15 14:54:14 -0500

    feat(model): the user model was added

--- src/main/java/com/biblioteca/model/UserModel.java
+++ src/main/java/com/biblioteca/model/UserModel.java
@@ -1,5 +1,39 @@
 package com.biblioteca.model;
 
+import java.util.List;
+
+import com.biblioteca.entity.User;
+import com.biblioteca.repository.UserRepository;
+
 public class UserModel {
 
+    private final UserRepository repository;
+
+    public UserModel(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<User> getAllUsers() {
+        return repository.findAll();
+    }
+
+    public void saveUser(User user) {
+        repository.save(user);
+    }
+
+    public void updateUser(User user) {
+        repository.update(user);
+    }
+
+    public void deleteUser(String email) {
+        repository.delete(email);
+    }
+
+    public boolean existsByEmail(String email) {
+        return repository.existsByEmail(email);
+    }
+
+    public User findByEmail(String email) {
+        return repository.findByEmail(email);
+    }
 }
